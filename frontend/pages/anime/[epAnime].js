66149719b952d9dceda1a6060399dd336489e5c4
Alexandre Silva <xande1231231@hotmail.com>
2021-04-28 08:48:09 -0300

    Fix

--- frontend/pages/anime/[epAnime].js
+++ frontend/pages/anime/[epAnime].js
@@ -1,292 +1,392 @@
 import Head from 'next/head'
 import axios from 'axios';
+import React from 'react'
 import { useState } from 'react';
 import { useRouter } from 'next/router'
+import Carousel from 'react-elastic-carousel';
 
-function Anime({anime, error}) {
-    const [state, setState] = useState([]);
-    state: {
-      hoverLeft: false
+class Anime extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      anime: {
+        animeNome: null
+      }
+    };
+  }
+  componentDidMount(){
+    this.getAnimes()
+  }
+  async getAnimes() {
+    const animeID =document.location.pathname.split('/')[2] 
+    const react = this
+    try {
+      const res = await axios.get(`http://localhost:3333/anime/${animeID}`);
+      const json = res.data
+      react.setState({anime: json})
+      console.log(react.state.anime)
+    } catch (error) {
+      console.log(`error`)
+      return { error };
     }
-    if (error) {
-      return <div>An error occured: {error.message}</div>;
-    }
-    return (
-    <div className="container">
-      <Head>
-        <title>NekoWatch - {anime.animeNome}</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
-      </Head>
-
+  }
+  render() {
+    return <div className="container">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+    <Head>
+      <title>NekoWatch - { this.state.anime.animeNome != null ? this.state.anime.animeNome : 'Carregando anime' }</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <div className="container_anime">
+      { this.state.anime.animeNome != null && 
       <div className="anime_container">
         <div className="imgCover">
           <a href="/" className="voltar">
             <i className="fas fa-arrow-left"></i>
           </a>
-            <img src={anime.imageCover}></img>
+            <img src={this.state.anime.imageCover}></img>
         </div>
         <div className="photoAnime">
-          <h1>{ anime.animeNome }</h1>
-          <p className="sinopse">{ anime.sinopse }</p>
-          <img src={anime.photo}></img>
+          <h1>{ this.state.anime.animeNome }</h1>
+          <p className="sinopse">{ this.state.anime.sinopse }</p>
+          <img src={this.state.anime.photo}></img>
         </div>
         <div className="scrollAnime">
         <h1 className="title">Episódios</h1>
-        {anime.idEpisode.map(ep => (
-          <a 
+        <Carousel 
+        itemsToShow={8}>
+        {this.state.anime.idEpisode.map(ep => (
+          <div>
+            <a 
           href={`/assistir/${ep.episodeId}`}>
             <div 
-            className="anime" key={ep.name}>
+            className="anime" key={ep.episodeId}>
               <div className="photo">
+                <div className="transparent"></div>
                 <img src={ep.image}/>  
               </div>
               <h1>{ ep.episode }</h1>
             </div>
-          </a> ))}
+          </a> 
+          </div>
+          ))}
+        </Carousel>
+        </div>
       </div>
-      </div>
+      }
       
-
-      <style jsx global>{`
-         html,
-         body {
-           padding: 0;
-           margin: 0;
-           font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-             Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-             sans-serif;
-           background-color: #181818;
-           overflow: hidden;
-         }
- 
-         .photoAnime{
-           position: relative;
-           left: 160px;
-           top: -80px;
-         }
-
-         .photoAnime img{
-            margin: 10px;
-            box-shadow: 1px 2px 3px #3f51b594;
-         }
-
-         .photoAnime h1{
-           color: #fff;
-           bottom: -60px;
-           position: absolute;
-           text-shadow: 1px 1px 2px rgba(0,0,0,.40);
-         }
-
-         .photoAnime img{
-          width: 170px;
-          object-fit: cover;
-          height: 220px;
-        }
-
-         * {
-           box-sizing: border-box;
-         }
- 
-         .scrollAnime{
-           overflow: hidden;
-           height: 200px;
-           white-space: nowrap;
-           z-index: 1;
-           position: relative;
-           top: -60px;
-         }
- 
-         .fixedLeft .logo{
-           position: absolute;
-           width: 100px;
-           height: 100px;
-           right: 0px;
-           float: right;
-         }
- 
-         .fixedLeft p{
-           display: inline-flex;
-         }
- 
-         .fixedLeft .logo img{
-           position: absolute;
-           width: 100px;
-           transform: rotate(270deg) translateY(53px);
-           transition: 0.4s;
-         }
- 
-         .fixedLeft .logo img:hover{
-           transform: rotate(270deg) translateY(0px);
-         }
- 
-         .FixadoLeft{
-           width: 421px !important;
-         }
- 
-         .fixedLeftNot{
-           width: 90px !important;
-         }
- 
-         .fixedLeft{
-           position: fixed;
-           top: 0px;
-           left: 0px;
-           width: 90px;
-           height: 100%;
-           background: #181818;
-           overflow: hidden;
-           z-index: 10;
-           box-shadow: 1px 2px 1px rgb(47 43 43 / 41%);
-         }
- 
-         .fixedLeft li{
-           list-style: none;
-           color: white;
-           padding: 10px;
-           font-size: 180%;
-           position: relative;
-           top: 100px;
-         }
- 
-         .dontFixado li span i{
-           position: relative;
-           left: -50px !important;
-         }
- 
-         .dontFixado li p{
-           display: none;
-         }
- 
-         .fixedLeftNot li i{
-           display: block;
-           position: relative;
-           left: 10px !important;
-         }
- 
-         .fixedLeft li span{
-           margin-left: 70px;
-         }
- 
-         .fixedLeft li span i{
-           position: relative;
-           left: -20px;
-         }
- 
-         .fixedLeft .ativo{
-           background: white;
-           color: #181818;
-         }
-         
-         .title{
-           color: #fff;
-           position: relative;
-           left: 48px;
-         }
- 
-         .scrollAnime{
-           width: 8540px;
-           height: auto;
-           overflow: hidden;
-           opacity: 1;
-           transform: translate3d(0px, 0px, 0px);
-           margin: 10px;
-           position: relative;
-           left: -40px;
-         }
- 
-         .scrollAnime h1{
-           position: relative;
-           left: 185px;
-           top: 30px;
-         }
-         
-         .anime{
-           width: 22vw;
-           height: 22vh;
-           display: inline-block;
-           transform: scale(0.8);
-           position: relative;
-           left: 230px;
-           border-radius: 4px;
-           cursor: pointer;
-           margin-left: -4vw;
-         }
- 
-         .anime .photo{
-           position: absolute;
-           width: 100%;
-           height: 100%;
-         }
- 
-         .anime img{
-           width: 100%;
-           height: 100%;
-           object-fit: cover;
-         }
-
-         .anime h1{
-           position: absolute;
-           margin-top: 4.4em;
-           color: #fff;
-           left: -1em;
-           font-size: 2vw;
-           transform: scale(0.8);
-           text-shadow: 1px 2px 1px rgba(0,0,0,.40);
-         }
-
-         .anime .imgCover{
-           width: 100%;
-           height: 400px;
-           position: fixed;
-           left: 0px;
-         }
-
-         .imgCover img{
-           width: 100%;
-           height: 300px;
-           object-fit: cover;
-         }
-
-         .voltar{
-           position: fixed;
-           text-align: center;
-           z-index: 100;
-           background: #fff;
-           color: #181818;
-           margin: 40px;
-           padding: 10px;
-           border-radius: 50%;
-           width: 40px;
-           height: 40px;
-           transform: scale(1.4);
-           box-shadow: 1px 2px 1px rgba(0,0,0,.50);
-           cursor: pointer;
-         }
-
-         .sinopse{
-           color: #fff;
-           max-width: 40%;
-           position: absolute;
-           top: 70px;
-           left: 200px;
-           color: rgba(255,255,255,.80);
-           height: 141px;
-           overflow: auto;
-         }
-      `}</style>
     </div>
-  )
-}
 
-Anime.getInitialProps = async ({ctx, query}) => {
-  try {
-    const res = await axios.get(`http://localhost:3333/anime/${query.epAnime}`);
-    const anime = res.data;
-    return { anime };
-  } catch (error) {
-    return { error };
+    <style jsx global>{`
+      html,
+      body {
+        padding: 0;
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+          Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+          sans-serif;
+        background-color: #181818;
+        user-select: none;
+        overflow-y: auto;
+        overflow-x: hidden;
+      }
+
+      * {
+        box-sizing: border-box;
+      }
+
+      .scrollAnime{
+        transform: scale(0.85);
+      }
+
+      .fixedLeft .logo{
+        position: absolute;
+        width: 100px;
+        height: 100px;
+        right: 0px;
+        float: right;
+      }
+
+      .fixedLeft p{
+        display: inline-flex;
+      }
+
+      .fixedLeft .logo img{
+        position: absolute;
+        width: 100px;
+        transform: rotate(270deg) translateY(53px);
+        transition: 0.4s;
+      }
+
+      .fixedLeft .logo img:hover{
+        transform: rotate(270deg) translateY(0px);
+      }
+
+      .FixadoLeft{
+        width: 421px !important;
+      }
+
+      .fixedLeftNot{
+        width: 90px !important;
+      }
+
+      .fixedLeft{
+        position: fixed;
+        top: 0px;
+        left: 0px;
+        width: 90px;
+        height: 100%;
+        background: #181818;
+        overflow: hidden;
+        z-index: 10;
+        box-shadow: 1px 2px 1px rgb(47 43 43 / 41%);
+      }
+
+      .fixedLeft li{
+        list-style: none;
+        color: white;
+        padding: 10px;
+        font-size: 180%;
+        position: relative;
+        top: 100px;
+      }
+
+      .dontFixado li span i{
+        position: relative;
+        left: -50px !important;
+      }
+
+      .dontFixado li p{
+        display: none;
+      }
+
+      .fixedLeftNot li i{
+        display: block;
+        position: relative;
+        left: 10px !important;
+      }
+
+      .fixedLeft li span{
+        margin-left: 70px;
+      }
+
+      .fixedLeft li span i{
+        position: relative;
+        left: -20px;
+      }
+
+      .fixedLeft .ativo{
+        background: white;
+        color: #181818;
+      }
+      
+      .title{
+        color: #fff;
+        position: relative;
+        left: 64px;
+      }
+
+      .scrollAnime{
+        margin-left: -40px;
+      }
+      
+      .anime{
+        width: 210px;
+        height: 250px;
+        display: inline-block;
+      }
+
+      .anime .photo{
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        left: 0px
+      }
+
+      .rec-carousel-item:hover{
+        border: 2px solid #fff;
+      }
+
+      .anime img{
+        width: calc(100% - 4px);
+        height: 100%;
+        object-fit: cover;
+        margin-left: -1px;
+      }
+
+      .anime h1{
+        position: absolute;
+        margin-top: 10em;
+        color: #fff;
+        left: 10px;
+        font-size: 1.2vw;
+        text-shadow: 1px 2px rgba(0,0,0,.90);
+        max-width: 80%;
+        overflow: hidden;
+      }
+
+      .rec-carousel-item{
+        width: 200px;
+        left: 2vw;
+        position: relative;
+        margin-left: 5px;
+        border: 2px solid transparent;
+      }
+
+      .rec-arrow-left{
+        margin-left: 50px;
+      }
+
+      .photoAnime{
+        position: relative;
+        left: 160px;
+        top: -80px;
+      }
+      .photoAnime img{
+         margin: 10px;
+         box-shadow: 1px 2px 3px #3f51b594;
+      }
+      .photoAnime h1{
+        color: #fff;
+        bottom: -60px;
+        position: absolute;
+        text-shadow: 1px 1px 2px rgba(0,0,0,.40);
+      }
+      .photoAnime img{
+       width: 170px;
+       object-fit: cover;
+       height: 220px;
+     }
+      * {
+        box-sizing: border-box;
+      }
+
+      .fixedLeft .logo{
+        position: absolute;
+        width: 100px;
+        height: 100px;
+        right: 0px;
+        float: right;
+      }
+
+      .fixedLeft p{
+        display: inline-flex;
+      }
+
+      .fixedLeft .logo img{
+        position: absolute;
+        width: 100px;
+        transform: rotate(270deg) translateY(53px);
+        transition: 0.4s;
+      }
+
+      .fixedLeft .logo img:hover{
+        transform: rotate(270deg) translateY(0px);
+      }
+
+      .FixadoLeft{
+        width: 421px !important;
+      }
+
+      .fixedLeftNot{
+        width: 90px !important;
+      }
+
+      .fixedLeft{
+        position: fixed;
+        top: 0px;
+        left: 0px;
+        width: 90px;
+        height: 100%;
+        background: #181818;
+        overflow: hidden;
+        z-index: 10;
+        box-shadow: 1px 2px 1px rgb(47 43 43 / 41%);
+      }
+
+      .fixedLeft li{
+        list-style: none;
+        color: white;
+        padding: 10px;
+        font-size: 180%;
+        position: relative;
+        top: 100px;
+      }
+
+      .dontFixado li span i{
+        position: relative;
+        left: -50px !important;
+      }
+
+      .dontFixado li p{
+        display: none;
+      }
+
+      .fixedLeftNot li i{
+        display: block;
+        position: relative;
+        left: 10px !important;
+      }
+
+      .fixedLeft li span{
+        margin-left: 70px;
+      }
+
+      .fixedLeft li span i{
+        position: relative;
+        left: -20px;
+      }
+
+      .fixedLeft .ativo{
+        background: white;
+        color: #181818;
+      }
+      
+      .title{
+        color: #fff;
+        position: relative;
+        left: 48px;
+      }
+
+      .anime .imgCover{
+        width: 100%;
+        height: 400px;
+        position: fixed;
+        left: 0px;
+      }
+      .imgCover img{
+        width: 100%;
+        height: 300px;
+        object-fit: cover;
+      }
+      .voltar{
+        position: fixed;
+        text-align: center;
+        z-index: 100;
+        background: #fff;
+        color: #181818;
+        margin: 40px;
+        padding: 10px;
+        border-radius: 50%;
+        width: 40px;
+        height: 40px;
+        transform: scale(1.4);
+        box-shadow: 1px 2px 1px rgba(0,0,0,.50);
+        cursor: pointer;
+      }
+      .sinopse{
+        color: #fff;
+        max-width: 40%;
+        position: absolute;
+        top: 70px;
+        left: 200px;
+        color: rgba(255,255,255,.80);
+        height: 141px;
+        overflow: auto;
+      }
+    `}</style>
+  </div>
   }
-};
+}
 
 export default Anime
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,57 +1,78 @@
 import Head from 'next/head'
+import React from 'react'
 import axios from 'axios';
+import { createStore } from 'redux'
 import { useState } from 'react';
 import { useRouter } from 'next/router'
 import Carousel from 'react-elastic-carousel';
 
-const Links = [
-  {
-    "name": "Início",
-    "icon": "fa-home",
-    "ativo": true
-  }
-]
+var randomPage = Math.floor(Math.random() * 10) + 1
 
-const Home = ({animes, error}) => {
-  const [state, setState] = useState([]);
-  state: {
-    hoverLeft: false
-    Next: 0
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      animes: null,
+      atualSlide: 0,
+      animeDestaque: null
+    };
+    this.end= this.end.bind(this);
   }
-  function scrollLeft(){
-    Next++
+  componentDidMount(){
+    this.setState({animes: []})
+    this.getAnimes()
   }
-  function scrollRight(){
-    Next--
+  async getAnimes() {
+    const react = this
+    try {
+      const res = await axios.get(`http://localhost:3333/page/${randomPage}`);
+      const json = res.data
+      react.setState({animeDestaque: json.animes[Math.floor(Math.random() * json.animes.length)]})
+      react.setState({animes: json.animes})
+    } catch (error) {
+      console.log(`error`)
+      return { error };
+    }
   }
-  return (
-    <div className="container">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
-      <Head>
-        <title>NekoWatch</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  async end(e){
+    this.setState({atualSlide: this.state.atualSlide += 1})
+    console.log(this.state.atualSlide)
+    if(this.state.atualSlide == 3){
+      const react = this
+      randomPage++
+      try {
+        const res = await axios.get(`http://localhost:3333/page/${randomPage}`);
+        const json = res.data
+        var joined = react.state.animes.concat(json.animes);
+        react.setState({ animes: joined })
+        this.setState({atualSlide: 0})
+      } catch (error) {
+        console.log(`error`)
+        return { error };
+      }
+    }
+  }
+  render() {
+    return <div className="container">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+    <Head>
+      <title>NekoWatch</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <div className="animeDestaque">
 
-      <div 
-      // onMouseEnter={() => setState({hoverLeft: true})}
-      // onMouseLeave={() => setState({hoverLeft: false})}
-      className={`fixedLeft ${(state.hoverLeft ? `FixadoLeft` : 'dontFixado')}`}>
-        <div className="logo">
-          <img src={`./logo.png`}></img>
-        </div>
-        { Links.map((link, i) => 
-        <li className={`${(link.ativo ? 'ativo' : '')}`}>
-          <span>
-            <i className={`fas ${link.icon}`}></i>  <p>{ link.name }</p>
-          </span>
-        </li>
-        ) }
-      </div>
-      <div className="scrollAnime">
-        <h1 className="title">Animes</h1>
-        <div>
-        {animes.animes.map(anime => (
-          <a 
+    </div>
+    <div className="scrollAnime">
+      <h1 className="title">Animes</h1>
+      <div>
+        {
+        this.state.animes != null &&
+        <Carousel 
+        itemsToShow={8}
+        onNextEnd={this.end}>
+        {this.state.animes.map(anime => (
+          <div>
+            <a 
           href={`/anime/${anime.idAnime}`}>
             <div 
             className="anime" key={anime.name}>
@@ -61,199 +82,172 @@ const Home = ({animes, error}) => {
               </div>
               <h1>{ anime.name }</h1>
             </div>
-          </a> ))}
-        </div>
+          </a> 
+          </div>
+          ))}
+        </Carousel>
+        }
       </div>
-      
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-          background-color: #181818;
-          user-select: none;
-          overflow-y: auto;
-          overflow-x: hidden;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-
-        .fixedLeft .logo{
-          position: absolute;
-          width: 100px;
-          height: 100px;
-          right: 0px;
-          float: right;
-        }
-
-        .fixedLeft p{
-          display: inline-flex;
-        }
-
-        .fixedLeft .logo img{
-          position: absolute;
-          width: 100px;
-          transform: rotate(270deg) translateY(53px);
-          transition: 0.4s;
-        }
-
-        .fixedLeft .logo img:hover{
-          transform: rotate(270deg) translateY(0px);
-        }
-
-        .FixadoLeft{
-          width: 421px !important;
-        }
-
-        .fixedLeftNot{
-          width: 90px !important;
-        }
-
-        .fixedLeft{
-          position: fixed;
-          top: 0px;
-          left: 0px;
-          width: 90px;
-          height: 100%;
-          background: #181818;
-          overflow: hidden;
-          z-index: 10;
-          box-shadow: 1px 2px 1px rgb(47 43 43 / 41%);
-        }
-
-        .fixedLeft li{
-          list-style: none;
-          color: white;
-          padding: 10px;
-          font-size: 180%;
-          position: relative;
-          top: 100px;
-        }
-
-        .dontFixado li span i{
-          position: relative;
-          left: -50px !important;
-        }
-
-        .dontFixado li p{
-          display: none;
-        }
-
-        .fixedLeftNot li i{
-          display: block;
-          position: relative;
-          left: 10px !important;
-        }
-
-        .fixedLeft li span{
-          margin-left: 70px;
-        }
-
-        .fixedLeft li span i{
-          position: relative;
-          left: -20px;
-        }
-
-        .fixedLeft .ativo{
-          background: white;
-          color: #181818;
-        }
-        
-        .title{
-          color: #fff;
-          position: relative;
-          left: 48px;
-        }
-
-        .scrollAnime{
-          margin-left: -40px;
-        }
-
-        .scrollAnime h1{
-          position: relative;
-          left: 185px;
-          top: 30px;
-        }
-        
-        .anime{
-          width: 22vw;
-          height: 22vh;
-          display: inline-block;
-          transform: scale(0.8);
-          position: relative;
-          left: 230px;
-          border-radius: 4px;
-          cursor: pointer;
-          margin-left: -4vw;
-        }
-
-        .anime .photo{
-          position: absolute;
-          width: 100%;
-          height: 100%;
-        }
-
-        .anime img{
-          width: 100%;
-          height: 100%;
-          object-fit: cover;
-        }
-
-        .anime h1{
-          position: absolute;
-          margin-top: 8em;
-          color: #fff;
-          left: 0px;
-          font-size: 1.2vw;
-          text-shadow: 1px 2px rgba(0,0,0,.90);
-          max-width: 80%;
-          overflow: hidden;
-        }
-
-        .arrowLef{
-          position: absolute;
-          width: 100%;
-          top: 118px;
-          height: 17.6vh;
-        }
-
-        .arrowLef .arrowLeft{
-          height: 100%;
-          background: #fff;
-          width: 4vw;
-          left: 90px;
-          position: absolute;
-          z-index: 1000;
-        }
-
-        .arrowRight{
-          height: 100%;
-          background: #fff;
-          width: 4vw;
-          right: 0px;
-          position: absolute;
-          z-index: 1000;
-        }
-      `}</style>
     </div>
-  )
-}
+    
 
-Home.getInitialProps = async ctx => {
-  try {
-    var random = Math.floor(Math.random() * 10);
-    const res = await axios.get(`http://localhost:3333/page/${random}`);
-    const animes = res.data;
-    return { animes };
-  } catch (error) {
-    return { error };
+    <style jsx global>{`
+      html,
+      body {
+        padding: 0;
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+          Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+          sans-serif;
+        background-color: #181818;
+        user-select: none;
+        overflow-y: auto;
+        overflow-x: hidden;
+      }
+
+      * {
+        box-sizing: border-box;
+      }
+
+      .fixedLeft .logo{
+        position: absolute;
+        width: 100px;
+        height: 100px;
+        right: 0px;
+        float: right;
+      }
+
+      .fixedLeft p{
+        display: inline-flex;
+      }
+
+      .fixedLeft .logo img{
+        position: absolute;
+        width: 100px;
+        transform: rotate(270deg) translateY(53px);
+        transition: 0.4s;
+      }
+
+      .fixedLeft .logo img:hover{
+        transform: rotate(270deg) translateY(0px);
+      }
+
+      .FixadoLeft{
+        width: 421px !important;
+      }
+
+      .fixedLeftNot{
+        width: 90px !important;
+      }
+
+      .fixedLeft{
+        position: fixed;
+        top: 0px;
+        left: 0px;
+        width: 90px;
+        height: 100%;
+        background: #181818;
+        overflow: hidden;
+        z-index: 10;
+        box-shadow: 1px 2px 1px rgb(47 43 43 / 41%);
+      }
+
+      .fixedLeft li{
+        list-style: none;
+        color: white;
+        padding: 10px;
+        font-size: 180%;
+        position: relative;
+        top: 100px;
+      }
+
+      .dontFixado li span i{
+        position: relative;
+        left: -50px !important;
+      }
+
+      .dontFixado li p{
+        display: none;
+      }
+
+      .fixedLeftNot li i{
+        display: block;
+        position: relative;
+        left: 10px !important;
+      }
+
+      .fixedLeft li span{
+        margin-left: 70px;
+      }
+
+      .fixedLeft li span i{
+        position: relative;
+        left: -20px;
+      }
+
+      .fixedLeft .ativo{
+        background: white;
+        color: #181818;
+      }
+      
+      .title{
+        color: #fff;
+        position: relative;
+        left: 64px;
+      }
+
+      .scrollAnime{
+        margin-left: -40px;
+      }
+      
+      .anime{
+        width: 210px;
+        height: 250px;
+        display: inline-block;
+      }
+
+      .anime .photo{
+        position: absolute;
+        width: 100%;
+        height: 100%;
+      }
+
+      .rec-carousel-item:hover{
+        border: 2px solid #fff;
+      }
+
+      .anime img{
+        width: calc(100% - 4px);
+        height: 100%;
+        object-fit: cover;
+        margin-left: -1px;
+      }
+
+      .anime h1{
+        position: absolute;
+        margin-top: 10em;
+        color: #fff;
+        left: 10px;
+        font-size: 1.2vw;
+        text-shadow: 1px 2px rgba(0,0,0,.90);
+        max-width: 80%;
+        overflow: hidden;
+      }
+
+      .rec-carousel-item{
+        width: 200px;
+        left: 2vw;
+        position: relative;
+        margin-left: 5px;
+        border: 2px solid transparent;
+      }
+
+      .rec-arrow-left{
+        margin-left: 50px;
+      }
+    `}</style>
+  </div>
   }
-};
+}
 
 export default Home
